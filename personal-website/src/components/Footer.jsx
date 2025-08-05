17afa823d5f0401ce5baba32e93839fb804015af
Anu48 <anoushka212@gmail.com>
2025-08-05 16:04:33 -0400

    Add credit for template

--- personal-website/src/components/Footer.jsx
+++ personal-website/src/components/Footer.jsx
@@ -5,7 +5,13 @@ export const Footer = () => {
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       <p className="text-sm text-muted-foreground">
         anu48.github.io - {new Date().getFullYear()}
+        <br />
+        Special thanks to the following template{" "}
+        <a href="https://www.youtube.com/watch?v=ifOJ0R5UQOc" className="text-primary">
+          here
+        </a>
       </p>
+
       <a
         href="#hero"
         className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
